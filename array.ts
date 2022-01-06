3e3380556eb954f78fc7eaac57c4e3ef18cd39f3
playck <playck@naver.com>
2022-01-06 23:20:45 +0900

    object & array

---
+++ array.ts
@@ -0,0 +1,3 @@
+let list: (number | string)[] = [1,2,3,"4"] // 잘 사용되는 방식
+
+let list2: Array<number> = [1,2,3]
---
+++ object.ts
@@ -0,0 +1,5 @@
+const person1 = {name: 'Mark', age: 38}
+
+const person2 = Object.create({name: "Mark", age: 38})
+
+// object type은 원시타입이 아닌 것을 나타내고 싶을 떄 사용하는 타입
