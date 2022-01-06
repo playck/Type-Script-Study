bdd45e18ed094883a49589ec7e26ef737dd2ec64
playck <playck@naver.com>
2022-01-06 22:48:27 +0900

    number, string

---
+++ string.ts
@@ -0,0 +1,10 @@
+let myName: string = 'Mark';
+
+myName = 'Anna';
+
+let fullName: string = 'Mark Lee';
+let age: number = 39;
+
+let sentence: string = `Hello, My name is ${fullName}.`
+
+console.log(sentence)
